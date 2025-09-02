7adcf6f789770fa729013e6d6635d82e4de8e3c1
Jean Gustavo <jeangustavo@MacBook-Air-de-Jean.local>
2025-09-02 15:54:14 -0300

    Add favicon.png and Google Tag Manager integration

--- src/components/DynamicFavicon.tsx
+++ src/components/DynamicFavicon.tsx
@@ -1,20 +1,10 @@
 import { useEffect } from "react"
-import { useTheme } from "@/hooks/use-theme"
 
 const DynamicFavicon = () => {
-  const { shouldUseDarkLogo } = useTheme()
-
   useEffect(() => {
-    // Atualiza o favicon
-    const link = document.querySelector("link[rel*='icon']") as HTMLLinkElement
-    if (link) {
-      // Usa os arquivos da raiz do projeto
-      link.href = shouldUseDarkLogo ? "/favicon.png" : "/favicon-branco.png"
-    }
-
     // Atualiza o título da página
     document.title = "Swift Soft"
-  }, [shouldUseDarkLogo])
+  }, [])
 
   return null
 }
